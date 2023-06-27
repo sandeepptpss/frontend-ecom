664fa1d9d9b3e4d895a11de70b1eb74b82379fbd
sandeepptpss <sandeepptpss@gmail.com>
2023-06-27 22:57:52 +0530

    new update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,6 @@ const NavBar =()=>{
 <li><Link to="/">Home</Link></li>
 <li><Link to="/collections">Product</Link></li>
 <li><Link to="/add">add Product</Link></li>
-<li><Link to="/update">Update Product</Link></li>
 <li><Link to="/profile">Profile</Link></li>
 <Link onClick={logout} to="/signup">Logout</Link>
 </ul>
